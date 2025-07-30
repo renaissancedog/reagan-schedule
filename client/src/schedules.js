442f8f1706b527b1e6b2eb2aa7c13fba00cb057f
William Liu <orangetnt729@gmail.com>
2025-07-30 14:34:08 -0500

    update some schedules

--- client/src/schedules.js
+++ client/src/schedules.js
@@ -23,90 +23,88 @@ Time.prototype = {
     return x - y + (x > y ? 0 : 24 * 60);
   }
 };
-export const specialDates = [
-  [[[5, 15]], 'Senior Exam Schedule 1'],
-  [[[5, 16]], 'Senior Exam Schedule 2'],
-  [[[5, 19]], 'Semester Exam Schedule 1'],
-  [[[5, 20]], 'Semester Exam Schedule 2'],
-  [[[5, 21]], 'Semester Exam Schedule 3'],
-  [[[5, 22]], 'Semester Exam Schedule 4']
-];
+export const specialDates = [];
 //schedules
 
 export const reg = [
-  { name: 'Period 1', id: 'p1', start: new Time(8, 50), end: new Time(9, 45) },
-  { name: 'Period 2', id: 'p2', start: new Time(9, 50), end: new Time(10, 45) },
+  { name: 'Period 1', id: 'p1', start: new Time(8, 55), end: new Time(9, 50) },
+  { name: 'Period 2', id: 'p2', start: new Time(9, 55), end: new Time(10, 50) },
   {
     name: 'Period 3',
     id: 'p3',
-    start: new Time(10, 50),
-    end: new Time(11, 40)
+    start: new Time(10, 55),
+    end: new Time(11, 45)
   },
   {
     name: 'Period 4',
     id: 'p4',
-    start: new Time(11, 45),
-    end: new Time(12, 35)
+    start: new Time(11, 50),
+    end: new Time(12, 40)
   },
   {
     name: 'Period 5',
     id: 'p5',
-    start: new Time(12, 40),
-    end: new Time(13, 30)
+    start: new Time(12, 45),
+    end: new Time(13, 35)
   },
   {
     name: 'Period 6',
     id: 'p6',
-    start: new Time(13, 35),
-    end: new Time(14, 25)
+    start: new Time(13, 40),
+    end: new Time(14, 30)
   },
   {
     name: 'Period 7',
     id: 'p7',
-    start: new Time(14, 30),
-    end: new Time(15, 20)
-  },
-  {
-    name: 'Period 8',
-    id: 'p8',
-    start: new Time(15, 25),
-    end: new Time(16, 15)
-  }
-];
-export const strike = [
-  { name: 'Period 1', id: 'p1', start: new Time(8, 50), end: new Time(9, 40) },
-  { name: 'Period 2', id: 'p2', start: new Time(9, 45), end: new Time(10, 30) },
-  {
-    name: 'STRIKE Time',
-    id: 'STRIKE',
-    start: new Time(10, 35),
-    end: new Time(11, 15)
-  },
-  { name: 'Period 3', id: 'p3', start: new Time(11, 20), end: new Time(12, 5) },
-  {
-    name: 'Period 4',
-    id: 'p4',
-    start: new Time(12, 10),
-    end: new Time(12, 55)
-  },
-  { name: 'Period 5', id: 'p5', start: new Time(13, 0), end: new Time(13, 45) },
-  {
-    name: 'Period 6',
-    id: 'p6',
-    start: new Time(13, 50),
-    end: new Time(14, 35)
-  },
-  {
-    name: 'Period 7',
-    id: 'p7',
-    start: new Time(14, 40),
+    start: new Time(14, 35),
     end: new Time(15, 25)
   },
   {
     name: 'Period 8',
     id: 'p8',
     start: new Time(15, 30),
-    end: new Time(16, 15)
+    end: new Time(16, 20)
+  }
+];
+export const strike = [
+  { name: 'Period 1', id: 'p1', start: new Time(8, 55), end: new Time(9, 45) },
+  { name: 'Period 2', id: 'p2', start: new Time(9, 50), end: new Time(10, 35) },
+  {
+    name: 'STRIKE Time',
+    id: 'STRIKE',
+    start: new Time(10, 40),
+    end: new Time(11, 20)
+  },
+  {
+    name: 'Period 3',
+    id: 'p3',
+    start: new Time(11, 25),
+    end: new Time(12, 10)
+  },
+  {
+    name: 'Period 4',
+    id: 'p4',
+    start: new Time(12, 15),
+    end: new Time(13, 0)
+  },
+  { name: 'Period 5', id: 'p5', start: new Time(13, 5), end: new Time(13, 50) },
+  {
+    name: 'Period 6',
+    id: 'p6',
+    start: new Time(13, 55),
+    end: new Time(14, 40)
+  },
+  {
+    name: 'Period 7',
+    id: 'p7',
+    start: new Time(14, 45),
+    end: new Time(15, 30)
+  },
+  {
+    name: 'Period 8',
+    id: 'p8',
+    start: new Time(15, 35),
+    end: new Time(16, 20)
   }
 ];
 export const er = [
